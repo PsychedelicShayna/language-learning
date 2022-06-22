7c869f6af67b5f338ab3148046a914c85f7f41cf
PsychedelicShayna <0x0000@tuta.io>
2022-06-22 10:22:30 -0300

    Added new episode files

--- source/haskell-for-imperative-programmers/5-higher-orders.hs
+++ source/haskell-for-imperative-programmers/5-higher-orders.hs
@@ -26,14 +26,37 @@ add1Lambda :: Int -> Int
 add1Lambda = (\x -> x + 1)
 
 
--- A built-in higher order function "map", which works like it does in Python and
--- other languages that support functional programming concepts. It allows you
--- to run each element of a list through a lambda, returning a new list with 
--- eache lement mapepd to the return value when passed through the lambda.
--- The type of the output list isn't restricted by the type of the input list.
 
-add1ToList :: [Int] -> [Int]
-add1ToList xs = map (\x -> x + 1) xs
+-- Haskell naturally has a "map" function, which appears in other languages with
+-- partial support for functional programming, such as Python, Ruby, C++, etc.
+-- Haskell actually pioneered a lot of these functional programming patterns.
+-- Haskell's "map" function works as you would expect, it takes a function
+-- and a list, then returns a new list where each element of the previous 
+-- list is mapped to the return value of the function given that element.
+
+-- Type of the output list is not restricted to that of the input list.
+-- For example, here is a function that returns a list of booleans from
+-- a list of integers, specifying whether or not that integer is even.
+
+getIntsEven :: [Int] -> [Bool]
+getIntsEven xs = map (\n -> even n) xs
+
+-- Similarly, there is a filter function, which works just as you would
+-- expect from other languages. Elements are filtered out of a list based
+-- on the return value of a predicate function.
+
+filterOutOdds :: [Int] -> [Int]
+filterOutOdds xs = filter (\e -> even e) xs
+
+
+-- Another important higher order function is "fold", aka reduce.
+-- The first argument being a binary function, the second being
+-- the initial value, the third being the list.
+
+sumIntList :: [Int] -> Int
+sumIntList xs = foldr (+) 0 xs
+
+
 
 
 
